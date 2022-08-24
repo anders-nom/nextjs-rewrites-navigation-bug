0974c058208b5944e389c286478a023f23e6506a
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-08-24 11:10:37 +0200

    Add some describing text + try newest canary

--- pages/[[...myRoute]].js
+++ pages/[[...myRoute]].js
@@ -11,13 +11,17 @@ const MyComponent = ({ route }) => {
             <Link href={"/page2"}>
                 {'Page 2'}
             </Link>
+            <br/><br/>
             <div>{`Current page: ${route}`}</div>
+            <br/><br/>
+            <div>{'To reproduce the bug: Click a link, refresh the page (F5) and navigate back with the back button.' +
+                ' Observe that the page props does not update to correspond to the previous page, if any rewrites are present in next.config.js'}</div>
         </div>
     )
 }
 
 export const getStaticProps = async (context) => {
-    const route = context?.params?.myRoute.join('/');
+    const route = context?.params?.myRoute?.join?.('/') || '/';
 
     return {
         props: { route },
